dbb7b5818a24eaff754645818395ce973b7ecd28
Andrew Zhukov <andrewzhukovweb@gmail.com>
2022-12-18 01:01:34 +0300

    sqlite connection

--- backend/server.go
+++ backend/server.go
@@ -4,9 +4,15 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
